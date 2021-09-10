6de8f88e139233fac64b039087e0130bd6d95dc5
davedkg <davedkg@gmail.com>
2021-09-10 06:31:27 -0400

    A user can create and update a Post using an AJAX modal powered by StimulusJS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  layout 'modal', only: [:new, :edit]
 
   # GET /posts or /posts.json
   def index
@@ -27,9 +28,11 @@ class PostsController < ApplicationController
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
+        format.js { redirect_to posts_path, notice: "Post was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js { render :create }
       end
     end
   end
@@ -40,9 +43,11 @@ class PostsController < ApplicationController
       if @post.update(post_params)
         format.html { redirect_to @post, notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
+        format.js { redirect_to posts_path, notice: "Post was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js { render :update }
       end
     end
   end
